6e27500d5b1991e0dc13eed368ef68ec0c5303ac
Alessandro Sanino <saninoale@gmail.com>
2013-02-27 10:35:16 +0100

    Sanino Alessandro 27-03-2013
    Aggiunta e Aggiornamento ChatAgent, Server, Client, SqliteManager, sqlite3.h.
    Mancano SocketTCP, SERVER e CLient TCP!!!

--- Client.hpp
+++ Client.hpp
@@ -1,10 +1,48 @@
 /**
     20-02-2013
     Sanino Alessandro - Create
+
+    27-02-2013
+	Sanino Alessandro - Aggiunta Metodi
+
 **/
 
+#ifndef CLIENT
+#define CLIENT
+
 #include "ChatAgent.hpp"
+#include "ClientTCP.hpp"
 
 class Client : ChatAgent {
-    
+	private:
+	char* ServerIp;
+	int ServerPort;
+	ClientTCP *binary;
+
+	public:
+	sealed override bool Invia(char* Messaggio);
+	sealed override char* Ricevi();
+
+	Client(char* myIp, int myPort, char* ServerIp, int ServerPort);
+	~Client();
 }
+
+Client::Client(char* myIp, int myPort, char* ServerIp, int ServerPort) : ChatAgent(myIp, myPort) {
+	this->ServerIp = ServerIp;
+	this->ServerPort = ServerPort;
+}
+
+Client::~Client() {
+	free(this->ServerIp);
+	delete(binary);
+}
+
+sealed override bool Client::Invia(char* Messaggio) {
+	return this->binary->Invia(Messaggio);
+}
+
+sealed override char* Client::Ricevi() {
+	return (this->binary->Ricevi());
+}
+
+#endif
